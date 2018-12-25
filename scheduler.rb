f62f2b562292471c4378aa2010241dcb6a9a914c
chipset508 <tu.truonganh508@gmail.com>
2018-12-25 11:15:14 +0700

    Only delete old records

--- scheduler.rb
+++ scheduler.rb
@@ -1,5 +1,15 @@
 require './config/environment.rb'
 
 puts "Before GithubComment count: #{GithubComment.count}"
-GithubComment.destroy_all
+if GithubComment.count > 3000
+  GithubComment.order('id ASC').limit(500).destroy_all
+end
 puts "After GithubComment count: #{GithubComment.count}"
+
+puts "="*100
+
+puts "Before PullRequest count: #{PullRequest.count}"
+if PullRequest.count > 2000
+  PullRequest.order('id asc').limit(1000).destroy_all
+end
+puts "After PullRequest count: #{PullRequest.count}"
