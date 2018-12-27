3aa79884e91bed5e57deb4cad182275f84f803fb
chipset508 <tu.truonganh508@gmail.com>
2018-12-27 14:32:23 +0700

    Only find comment

--- app/models/github_comments.rb
+++ app/models/github_comments.rb
@@ -1,6 +1,6 @@
 class GithubComment < ActiveRecord::Base
   def first_comment_in_thread?
-    GithubComment.where(thread_ts: thread_ts, author_name: author_name).count == 1
+    GithubComment.where(thread_ts: thread_ts, author_name: author_name, state: nil).count == 1
   end
 
   def author_of_thread?
