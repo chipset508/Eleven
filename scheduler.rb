161e21f7e4ba99901995796609e4294e5324e902
chipset508 <tu.truonganh508@gmail.com>
2019-02-09 15:36:05 +0700

    Change delete pull request limit

--- scheduler.rb
+++ scheduler.rb
@@ -1,7 +1,7 @@
 require './config/environment.rb'
 
 puts "Before GithubComment count: #{GithubComment.count}"
-if GithubComment.count > 3000
+if GithubComment.count > 4000
   GithubComment.order('id ASC').limit(500).destroy_all
 end
 puts "After GithubComment count: #{GithubComment.count}"
@@ -9,7 +9,7 @@ puts "After GithubComment count: #{GithubComment.count}"
 puts "="*100
 
 puts "Before PullRequest count: #{PullRequest.count}"
-if PullRequest.count > 2000
+if PullRequest.count > 5000
   PullRequest.order('id asc').limit(1000).destroy_all
 end
 puts "After PullRequest count: #{PullRequest.count}"
